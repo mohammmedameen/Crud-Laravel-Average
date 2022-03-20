66f5e97d12be22a942e937066cc78d19296b11a6
mohammmedameen <bbackenddeveloper@gmail.com>
2022-03-20 03:37:20 +0200

    Create seeder and factory

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'category_id' => rand(1,10),
+            'name' => $this->faker->unique()->word(),
+            'description' => $this->faker->text(20),
+            'price' => rand(1,10),
+            'image' => "no image",
+            'slug' => $this->faker->unique()->word(),
+        ];
+    }
+}
+
